23faa6b967e59520e9f53933f3add60397ae327f
BibekBhusal0 <138126735+BibekBhusal0@users.noreply.github.com>
2024-10-06 11:01:29 +0545

    installind libraries and works

--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
-import '@pages/newtab/Newtab.css';
+import React from "react";
+import logo from "@assets/img/logo.svg";
+import "@pages/newtab/Newtab.css";
 
 export default function Newtab(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/newtab/Newtab.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+        <div className="text-xl">This is a new tab</div>
       </header>
     </div>
   );
