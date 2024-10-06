80238006348fb4e5d752bed658454b8ad26f1242
BibekBhusal0 <138126735+BibekBhusal0@users.noreply.github.com>
2024-10-06 12:03:00 +0545

    baisc setup with newtab optioins and popup page

--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import logo from "@assets/img/logo.svg";
-import "@pages/newtab/Newtab.css";
-
 export default function Newtab(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
         <div className="text-xl">This is a new tab</div>
+        <div className="text-xl">
+          Is new tab changing as I make changes in my file
+        </div>
       </header>
     </div>
   );
